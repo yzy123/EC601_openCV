556e34f338fda4d128775af8b919b8a96630863a
yzy123 <31748059+yzy123@users.noreply.github.com>
2017-11-06 11:59:55 -0500

    Update ex3.py

--- openCV/ex3.py
+++ openCV/ex3.py
@@ -27,7 +27,7 @@ def add_salt_pepper_noise(imag,pa,pb):
  
 image = cv2.imread('/Users/yangzhiyi/Desktop/openCV/Test_images/Lenna.png',0)
 
-cv2.imshow('Original_Image',image)
+cv2.imwrite('Original_Image.jpg',image)
 
 
 kernel = [(3,3),(5,5),(7,7)]
@@ -39,29 +39,29 @@ for k in range(0,3):
     for i in range(0,4):
         for j in range(0,4):
             nimage = image.copy()
-            name = 'Gaussian_Noise mean: %d, sigma: %d' % (mean[i],sigma[j])
+            name = 'Gaussian_Noise mean: %d, sigma: %d_.jpg' % (mean[i],sigma[j])
             add_gaussian_noise(nimage,mean[i],sigma[j])
-            cv2.imshow(str(name),nimage)
+            cv2.imwrite(str(name),nimage)
 
             
             imageaf = nimage.copy()
-            name = 'Box_filter mean: %d, sigma: %d, kernel_size: %dx%d' % (mean[i],sigma[j],kernel[k][0],kernel[k][0])
+            name = 'Box_filter mean: %d, sigma: %d, kernel_size: %dx%d_.jpg' % (mean[i],sigma[j],kernel[k][0],kernel[k][0])
             cv2.blur(imageaf,kernel[k],imageaf)
-            cv2.imshow(str(name),imageaf)
+            cv2.imwrite(str(name),imageaf)
 
             
 
             imageaf1 = nimage.copy()    
-            name = 'Gaussian_filter mean: %d, sigma: %d, kernel_size: %dx%d' % (mean[i],sigma[j],kernel[k][0],kernel[k][0])
+            name = 'Gaussian_filter mean: %d, sigma: %d, kernel_size: %dx%d_.jpg' % (mean[i],sigma[j],kernel[k][0],kernel[k][0])
             cv2.GaussianBlur(nimage,kernel[k],1.5,imageaf1)
-            cv2.imshow(str(name),imageaf1)
+            cv2.imwrite(str(name),imageaf1)
 
             
 
             imageaf2 = nimage.copy()        
-            name = 'Median_filter mean: %d, sigma: %d, kernel_size: %dx%d' % (mean[i],sigma[j],kernel[k][0],kernel[k][0])
+            name = 'Median_filter mean: %d, sigma: %d, kernel_size: %dx%d_.jpg' % (mean[i],sigma[j],kernel[k][0],kernel[k][0])
             cv2.medianBlur(nimage,kernel[k][0],imageaf2)
-            cv2.imshow(str(name),imageaf2)
+            cv2.imwrite(str(name),imageaf2)
 
             
 
@@ -70,31 +70,31 @@ for k in range(0,3):
 
 pa = [0.01,0.03,0.05,0.4]
 pb = [0.01,0.03,0.05,0.4]
-for k in range (0,1):
-    for i in range(0,1):
-        for j in range(0,1):
+for k in range (0,3):
+    for i in range(0,4):
+        for j in range(0,4):
             nimage2 = image.copy()
-            name = 'Salt and Pepper Noise, pa: %f, pb: %f' % (pa[i],pb[j])
+            name = 'Salt and Pepper Noise, pa: %f, pb: %f_.jpg' % (pa[i],pb[j])
             add_salt_pepper_noise(nimage2,pa[i],pb[j])
-            cv2.imshow(str(name),nimage2)
+            cv2.imwrite(str(name),nimage2)
 
         
 
             imageaf3 = nimage2.copy()
-            name = 'Box filter, pa: %f, pb: %f, kernel_size: %dx%d' % (pa[i],pb[j],kernel[k][0],kernel[k][0])
+            name = 'Box filter, pa: %f, pb: %f, kernel_size: %dx%d_.jpg' % (pa[i],pb[j],kernel[k][0],kernel[k][0])
             cv2.blur(imageaf3,kernel[k],imageaf3)
-            cv2.imshow(str(name),imageaf3,)
+            cv2.imwrite(str(name),imageaf3,)
 
 
             imageaf4 = nimage2.copy()
-            name = 'Gaussian filter, pa: %f, pb: %f, kernel_size: %dx%d' % (pa[i],pb[j],kernel[k][0],kernel[k][0])
+            name = 'Gaussian filter, pa: %f, pb: %f, kernel_size: %dx%d_.jpg' % (pa[i],pb[j],kernel[k][0],kernel[k][0])
             cv2.GaussianBlur(imageaf4,kernel[k],1.5,imageaf4)
-            cv2.imshow(str(name),imageaf4)
+            cv2.imwrite(str(name),imageaf4)
 
 
             imageaf5 = nimage2.copy()
-            name = 'Median filter, pa: %f, pb: %f, kernel_size: %dx%d' % (pa[i],pb[j],kernel[k][0],kernel[k][0])
+            name = 'Median filter, pa: %f, pb: %f, kernel_size: %dx%d_.jpg' % (pa[i],pb[j],kernel[k][0],kernel[k][0])
             cv2.medianBlur(imageaf5,kernel[k][0],imageaf5)
-            cv2.imshow(str(name),imageaf5,)
+            cv2.imwrite(str(name),imageaf5,)
 
 cv2.waitKey(0)
